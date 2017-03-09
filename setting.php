84f73943fd771e1a14f015ab4d2f7fe6906c7b96
kenichi.tokuoka <kenichi@Ks-MacbookAir.local>
2017-03-09 21:28:55 +0900

    setting画面少し

--- setting.php
+++ setting.php
@@ -8,9 +8,47 @@ if (empty($_SESSION['login_session'])) {
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>setting画面</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </head>
+<body>
+<div class="container">
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th colspan="2">設定</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td class="col-md-1">標準ARMサーバー</td>
+				<td class="col-md-3">bbbb2</td>
+			</tr>
+			<tr>
+				<td class="col-md-1">データARMサーバー</td>
+				<td class="col-md-3">bbbb2</td>
+			</tr>
+			<tr>
+				<td class="col-md-1">ARMルーター番号</td>
+				<td class="col-md-3">bbbb2</td>
+			</tr>
+			<tr>
+				<td class="col-md-1">画像認識センサー番号</td>
+				<td class="col-md-3">bbbb2</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
 
-<table class="table">
-	<tr>
-		<th></th>
-	</tr>
-</table>
+
+
