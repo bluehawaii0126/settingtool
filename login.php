c01933e15708519f9256a387352c43c15ff9741e
root <root@localhost.localdomain>
2017-03-10 14:28:35 +0900

    update

--- login.php
+++ login.php
@@ -3,47 +3,51 @@
 session_start();
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/settingtool/config.php'); 
+include_once('./Skinny.php');
 
-if (file_exists('./loginlock')) {
-	$updateDate = filemtime('loginlock');
-	$diffTime = strtotime('now') - $updateDate;
-
-	if ($diffTime > $LOGIN_LOCKTIME) {
-		unlink('./loginlock');
-		exit;
-	} else {
-	    header('Location: index.php?error=3');
-	    exit;	
-	}
+if (file_exists('.loginlock')) {
+    $updateDate = filemtime('.loginlock');
+    $diffTime = strtotime('now') - $updateDate;
+    if ($diffTime > $LOGIN_LOCKTIME) {
+        unlink('.loginlock');
+    } else {
+        $out = array();
+        $out['errmsg'] = 'ログインロックされています。<br/>しばらくたってから再度お試しください';
+    }
+    $Skinny->SkinnyDisplay('views/index.html', $out);
+    exit;
 }
 
 $userid = $_POST['userid'];
 $password = $_POST['password'];
-$passfile = file('./.password');
-$failedFile = file('./.failed');
+$passfile = file('.password');
+$failedFile = file('.failed');
 
 $loginFailed = false;
 if ($userid != 'armadmin') {
     $loginFailed = true;
 }
 if ($password != $passfile[0]) {
-	$loginFailed = true;
+    $loginFailed = true;
 }
 
 if ($loginFailed) {
-	$headerString = 'Location: index.php?error=';
-	if (isset($failedFile[0]) && $failedFile[0] + 1 > 2) {
-	    touch('loginlock');
-		$headerString .= '2';
-	} else {
-		$newFailedCount = 1 + $failedFile[0];
-		$f = fopen('.failed', 'w');
+    $out = array();
+    if (isset($failedFile[0]) && $failedFile[0] + 1 > 2) {
+        touch('.loginlock');
+        $out['errmsg'] = '3回失敗したためロックされました。<br/>しばらくたってから再度お試しください';
+        $f = fopen('.failed', 'w');
+        fwrite($f, 0);
+        fclose($f);
+    } else {
+        $newFailedCount = 1 + $failedFile[0];
+        $f = fopen('.failed', 'w');
         fwrite($f, $newFailedCount);
         fclose($f);
-		$headerString .= '1';
-	}
-    header($headerString);
-    exit;		
+        $out['errmsg'] = 'idまたはpasswordに誤りがあります';
+    }
+    $Skinny->SkinnyDisplay('views/index.html', $out);
+    exit;
 }
 
 $f = fopen('.failed', 'w');
@@ -51,6 +55,6 @@ fwrite($f, 0);
 fclose($f);
 
 $_SESSION['login_session'] = 'loginsuccess';
-header('Location: ./setting.php');
-exit;	
+$Skinny->SkinnyDisplay('setting.php', $out);
+
 ?>
