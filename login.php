cb878f1b1673e26844d0118627fd7d912f2ba263
kenichi.tokuoka <kenichi@Ks-MacbookAir.local>
2017-03-14 01:30:08 +0900

    json_decodeがうまくいかない！　20170313　眠いのでここまで

--- login.php
+++ login.php
@@ -62,28 +62,34 @@ fclose($f);
 $_SESSION['login_session'] = 'loginsuccess';
 
 // 保存されている情報があれば表示　なければ初期設定表示
+$saveFileUri = 'data.json';
 $out = array();
 $out['schedules'] = ArmUtil::getSchedules();
 $defaultSchedule = ArmUtil::getD3();
 $out = array_merge($out, $defaultSchedule);
-if (!file_exists('data.json')) {
+if (!file_exists($saveFileUri)) {
     $out['default_arm_server'] = $DEFAULT_ARM_SERVER;
     $out['data_arm_server'] = $DATA_ARM_SERVER;
     $out['arm_router_no'] = $ARM_ROUTER_NO;
     $out['pic_sensor_no'] = $PIC_SENSOR_NO;
 } else {
-    $json = file_get_contents('data.json');
-    error_log(var_dump(json_decode($json, true)));
-    $out['default_arm_server'] = '1';
-    $out['data_arm_server'] = '1';
-    $out['arm_router_no'] = '1';
-    $out['pic_sensor_no'] = '1';
-    $out['select_months'] = '1';
-    $out['select_days'] = '1';
-    $out['select_weekdays'] = '1';
-    $out['select_hours'] = '1';
-    $out['select_minutes'] = '1';
-    $out['select_seconds'] = '1';
+    $json = file_get_contents($saveFileUri, true);
+    $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+    $saveData = json_decode($json, true);
+
+error_log($saveData);
+
+
+    $out['default_arm_server'] = $saveData['default_arm_server'];
+    $out['data_arm_server'] = $saveData['data_arm_server'];
+    $out['arm_router_no'] = $saveData['arm_router_no'];
+    $out['pic_sensor_no'] = $saveData['pic_sensor_no'];
+    // $out['select_months'] = '1';
+    // $out['select_days'] = '1';
+    // $out['select_weekdays'] = '1';
+    // $out['select_hours'] = '1';
+    // $out['select_minutes'] = '1';
+    // $out['select_seconds'] = '1';
 }
 
 $template = $twig->loadTemplate('setting.html');
